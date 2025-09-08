af78cffc65b1a3a9032d58b5db046a4803092bc0
coling01 <col.greenwood@btopenworld.com>
2025-09-08 09:03:13 +0100

    demo(openapi): add test to show that openApi models are generated

---
+++ src/test/java/uk/gov/hmcts/cp/config/OpenApiObjectsTest.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.cp.config;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.cp.openapi.model.ErrorResponse;
+import uk.gov.hmcts.cp.openapi.model.Judges;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OpenApiObjectsTest {
+
+    @Test
+    void shouldGenerateJudgesObject() {
+        Judges judges = Judges.builder().build();
+        assertThat(judges).hasOnlyFields("judiciary");
+    }
+
+    @Test
+    void shouldGenerateErrorResponseObject() {
+        ErrorResponse errorResponse = ErrorResponse.builder().build();
+        assertThat(errorResponse).hasOnlyFields("error",
+                "details",
+                "message",
+                "timestamp",
+                "traceId");
+    }
+}

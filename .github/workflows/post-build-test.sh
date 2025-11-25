e27c5db345cb2e058f4aa66ce68e762279a89b76
coling01 <colin.greenwood@hmcts.net>
2025-11-25 12:16:52 +0000

    chore: post build test to check bom.json

--- .github/workflows/post-build-test.sh
+++ .github/workflows/post-build-test.sh
@@ -1,10 +1,11 @@
-
-jar tvf ./build/libs/*.jar | grep "bom.json" >/dev/null
+bomfile="bom.json"
+jar tvf ./build/libs/*.jar | grep "$bomfile" >/dev/null
 if [[ $? -eq 0 ]]
 then
     echo "Found bom.json in jarfile"
     exit 0
 else
-    echo "Failed to find bom.json in jarfile"
+    jar tvf ./build/libs/*.jar
+    echo "Failed to find $bomfile in jarfile"
     exit 1
 fi
